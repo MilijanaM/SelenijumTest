8d58041200268d2be1434cca5af002b1376509ba
MilijanaM <64639260+MilijanaM@users.noreply.github.com>
2021-03-04 12:38:05 +0100

    Third commit

--- newproject/src/newpackage/SearchComputer.java
+++ newproject/src/newpackage/SearchComputer.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SearchComputer{
@@ -12,25 +13,28 @@ public class SearchComputer{
 	public static void main(String[] args) throws InterruptedException
 	
 	    {
-	    System.getProperty("webdriver.chrome.driver","C:\\Users\\Milijana\\git\\SelenijumTest\\newproject\\driver\\chromedriver.exe");   
+	    System.setProperty("webdriver.chrome.driver","C:\\Users\\Milijana\\git\\SelenijumTest\\newproject\\driver\\chromedriver.exe");   
 	    WebDriver driver= new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 	    
 	    //open the web application
 	    driver.navigate().to("http://computer-database.gatling.io/computers");
 	    driver.manage().window().maximize();
+	    Thread.sleep(2000);
 	    
 	    //fill in the search bar
 	    driver.findElement(By.xpath("//*[@id=\"searchbox\"]")).sendKeys("ACE");
+	    Thread.sleep(3000);
 	    driver.findElement(By.xpath("//*[@id=\"searchsubmit\"]")).click();
+	    Thread.sleep(5000);
 	    
-
-	    if (driver.findElement(By.xpath("//*[@id=\"main\"]/h1")).equals("6 computers found"))
-	        System.out.println("ACE word in name is found");
+	    WebElement Element1 = driver.findElement(By.xpath("//*[@id=\"main\"]/h1"));
+	    
+	   if (Element1.getText().equals("6 computers found"))
+	        System.out.println("Successful");
 		else 
 			System.out.println("Faild search");	
 		
-	    
 	    
 	    driver.quit();
 	    
